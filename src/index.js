7bfc52b61eed56e608b10430082128516673151a
Paulo Augusto Nogueira Foroni <paulo.foroni@linx.com.br>
2019-07-25 14:18:26 -0300

    finalizado modulo Autenticação

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./src/generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
 
 // 2 - O resolvers objeto é a implementação real do esquema GraphQL.
 const resolvers = {
-    Query: {
-        info: () => null, //`This is the API of a Hackernews Clone`
-        // 2
-        
-    },
-    Mutation: {
-        // 2
-        post: (root, args, context, info) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description,
-            })
-        },
-    },
-
-    // 3 - Pode ser omitido - Eles não são necessários porque o servidor GraphQL infere como eles são
-    /*Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-    }*/
-}
+    Query,
+    Mutation,
+    User,
+    Link
+  }
 
 // 3 - esquema e os resolvedores são empacotados e passados ​​para o GraphQLServer que é importado graphql-yoga
 const server = new GraphQLServer({
